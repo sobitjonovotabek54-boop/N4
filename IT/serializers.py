682e15969cf159ab86257c1c4ee9136c25b040ab
sobitjonovotabek54-boop <sobitjonovotabek54@gmail.com>
2026-07-27 08:25:00 +0500

    update-4

--- IT/serializers.py
+++ IT/serializers.py
@@ -1,25 +1,87 @@
-
+from django.contrib.auth import authenticate
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
 from .models import CustomUser
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    class Meta:
-        model=CustomUser
-        fields=['username','password','first_name','last_name','phone_number','bio','telegram_username',]
-        read_only_fields=['id']
-        
 
+    class Meta:
+        model = CustomUser
+        fields = [
+            "id",
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "bio",
+        ]
+        read_only_fields = ["id"]
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data["username"],
-            first_name=validated_data["first_name"],
-            last_name=validated_data["last_name"],
-            phone_number=validated_data["phone_number"],
-            bio=validated_data["bio"],
-            telegram_username=validated_data.get("telegram_username"),
-            password=validated_data["password"],
+        password = validated_data.pop("password")
+
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        user = authenticate(
+            username=attrs["username"],
+            password=attrs["password"],
         )
-        return user   
+
+        if not user:
+            raise serializers.ValidationError("Username yoki parol xato.")
+
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "bio",
+            "slug",
+            "created_at",
+        ]
+        read_only_fields = [
+            "id",
+            "slug",
+            "created_at",
+        ]
+
+
+class RefreshSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    def validate(self, attrs):
+        try:
+            token = RefreshToken(attrs["refresh"])
+
+            return {
+                "access": str(token.access_token)
+            }
+
+        except TokenError:
+            raise serializers.ValidationError("Refresh token yaroqsiz.")
